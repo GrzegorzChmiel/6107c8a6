7574405d67304fe59efe7595473eed394fc7c9b4
grzegorz.niedbala <grzegorz.niedbala@vsoft.pl>
2025-06-18 09:39:08 +0200

    Ćwiczenia do lekcji 4.

---
+++ Ćwiczenia/Lekcja 4 - Typy referencyjne/L4C1/L4C1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Start");
+Console.WriteLine();
+
+string name = "Jan";
+string surname = "Nowak";
+short age = 45;
+char sex = 'm';
+long pesel = 80081585553;
+long employeeNumer = 2509324094;
+
+Console.WriteLine($"Imie: {name}");
+Console.WriteLine($"Nazwisko: {surname}");
+Console.WriteLine($"Wiek: {age}");
+Console.WriteLine($"Plec: {sex}");
+Console.WriteLine($"Pesel: {pesel}");
+Console.WriteLine($"Numer pracownika: {employeeNumer}");
+
+Console.WriteLine();
+Console.WriteLine("Stop");
---
+++ Ćwiczenia/Lekcja 4 - Typy referencyjne/L4C4/L4C4/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Start");
+Console.WriteLine();
+
+short number1 = 10;
+string name = "Szkoła Dotneta";
+float number2 = 12.5f;
+
+Console.WriteLine($"Pierwsza liczba: {number1}");
+Console.WriteLine($"Napis: {name}");
+Console.WriteLine($"Druga liczba: {number2}");
+
+Console.WriteLine();
+Console.WriteLine("Stop");
